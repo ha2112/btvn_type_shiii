944a5c5d9cfb2c9b5092a3f8e4831519a929469d
ha2112 <trinhhonganh21122005@gmail.com>
2024-10-06 00:00:18 +0700

    testing updates

--- testing.cpp
+++ testing.cpp
@@ -3,42 +3,9 @@
 
 using namespace std;
 
-bool check(string a, string b){
-    unordered_map<char, int> mp;
-    int len = b.size();
-    for (int i = 0; i < len; i++){
-        mp[a[i]] += 1;
-        // cout << a[i] << " " << mp[a[i]] << endl;
-    }
-    for (int i = 0; i < len; i++){
-        mp[b[i]] -= 1;
-        if(mp[b[i]] < 0) return false;
-    }
-    return true;
-}
-
-bool checkInclusion(string s1, string s2) {
-    unordered_map<char, int> mp;
-    int len = s1.size();
-    for (char x : s1){
-        mp[x] += 1;
-    }
-    for (int i = 0; i < s2.size(); i++){
-        if(mp[s2[i]] != 0){
-            if (check(&s2[i], s1)) return true;
-        }
-    }
-    return false;
-}
-
 int main(){
-    unordered_map<char, int> mp;
-    string s1 = "dinitrophenylhydrazine";
-    string s2 = "acetylphenylhydrazine";
-    // cout << checkInclusion(s1, s2);
-    cout << s1.size() <<endl;
-    for (int i = 0; i < s1.size(); i++){
-        cout << s1[i] << " ";
+    if(1){
+        cout << "1 is true";
     }
     return 0;
 }   //stupid me
