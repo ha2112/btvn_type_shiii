f02271b6f5afcca2d7edf218c7c567c7267e1c4a
ha2112 <trinhhonganh21122005@gmail.com>
2024-10-14 00:39:45 +0700

    testing

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,6 @@
+// for (int i = max_index - max_r; i <= max_index + max_r; i++){
+    //     if(s2[i] != '#'){
+    //         ans += s2[i];
+    //         cout << s2[i] << " ";
+    //     }
+    // }
--- testing.cpp
+++ testing.cpp
@@ -1,21 +1,55 @@
-#include<iostream>
-#include<vector>
+#include<bits/stdc++.h>
 
 using namespace std;
 
-int main(){
-    vector<int> v;
-    int a[] = {03212,32214,214213, 421412};
-    int b[] = {100, 2193, 2131, 94032};
-    for (auto x : a){
-        v.push_back(x);
+bool legit(string s){
+    string a;
+    if (s[0] != '-'){
+        a = "2147483647";
+    } else {
+        a = "-2147483648";
     }
-    for (auto x : b){
-        v.push_back(x);
+    if (s.size() < a.size()) return true;
+    if (s.size() > a.size()) return false;
+    for(int i = 0; i < s.size(); i++){
+        if (a[i] > s[i]) return true;
+        else if (a[i] < s[i]) return false;
     }
-    sort(v.begin() , v.end());
-    for (auto x : v){
-        cout << x << " ";
+    return true;
+}
+
+
+int reverse(int x) {
+    string s = to_string(x);
+    cout << s << endl;
+    int start;
+    if (s[0] == '-'){
+            start = 1;
+    } else {
+        start = 0;
+    } 
+    int n = s.size();
+    for (int i = start; i <= n/2; i++){
+        swap(s[i], s[n-i-(1-start)]);
+        cout << i << endl;
     }
-    return 0;
+    int ans = 0;
+    cout << start << endl << s << endl;
+    if(legit(s)){
+        for (int i = start; i < n; i++){
+            ans *= 10;
+            ans += s[i] - '0';
+        }
+    }
+    if (start == 1){
+        return -ans;
+    } else {
+        return ans;
+    }
+}
+
+signed main(){
+    // char a = '0' - 47;
+    // cout << endl << a << endl;
+    cout << endl << reverse(-23) << endl;
 }
