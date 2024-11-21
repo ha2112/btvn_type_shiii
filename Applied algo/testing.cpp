4792fed4bb624dce742b74bf6d1b1c8cf509c8e2
ha2112 <trinhhonganh21122005@gmail.com>
2024-11-21 10:45:15 +0700

    Applied algo filler

--- Applied algo/testing.cpp
+++ Applied algo/testing.cpp
@@ -2,30 +2,30 @@
 
 using namespace std;
 
-const int N = 1e6 + 1;
-int a[N], S[N];
-int n, L1, L2, ans;
+const int N = 1e3 + 2, MOD = 1e9 + 7;
+int n, k1, k2, F[N][2], res;
 
 void input(){
     ios::sync_with_stdio(0);
     cin.tie(0);
-    cin >> n >> L1 >> L2;
-    for (int i = 1; i <= n; i++) cin >> a[i];
+    cin >> n >> k1 >> k2;
+}
+
+void proc(){
+    F[0][0] = F[0][1] = 1;
+    for (int i = 1; i <= n; i++){
+        for (int j = k1; j <= k2; j++){
+            if(i - j <  0) break;
+            F[i][1] += F[i - j][0];
+            F[i][1] %= MOD;
+        }
+        F[i][0] = F[i-1][1];
+    }
+    res = (F[n][0] + F[n][1]) % MOD;
+    cout << res << endl;
 }
 
 signed main(){
     input();
-    deque<int> d;
-    ans = 0;
-    for (int i = 1; i <= n; i++){
-        while(!d.empty() && d.front() < i - L2) d.pop_front();
-        int j = i - L1;
-        if (j >= 0) {
-            while(!d.empty() && S[d.back()] <= S[j]) d.pop_back();
-            d.push_back(j);
-        }
-        S[i] = a[i] + (d.empty() ? 0 : S[d.front()]);
-        ans = max(ans, S[i]);
-    }
-    cout << ans;
+    proc();
 }
